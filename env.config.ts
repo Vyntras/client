ea77cc63edfb8bc73b77c0fd6f3c5ac4d8d29efb
tommy fall <finished92for@gmail.com>
2024-08-03 18:47:59 -0400

    setup api strings

--- env.config.ts
+++ env.config.ts
@@ -1,3 +1,14 @@
 
 
-export const API = process.env.NEXT_PUBLIC_DOMAIN_API
+const dev = process.env.NODE_ENV == 'development'
+const isServer = typeof window === 'undefined';
+
+const setServer = () =>{
+    if(dev){
+        return process.env.NEXT_PUBLIC_DOMAIN_API
+    }else{
+        return isServer ? process.env.NEXT_PUBLIC_DOMAIN_API:'/api'
+    }
+}
+
+export const API = setServer()
--- src/app/components/CreateCardForm.tsx
+++ src/app/components/CreateCardForm.tsx
@@ -3,7 +3,7 @@
 import { Card } from "@/types/Card";
 import { FC, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form"
-import axios from 'axios'
+
 import { API } from "../../../env.config";
 
 
@@ -14,10 +14,16 @@ const CreateCardForm: FC = () => {
     const { register, handleSubmit } = useForm<Omit<Card, 'createdAt' | 'id'>>()
 
     const onSubmit: SubmitHandler<Omit<Card, 'createdAt' | 'id'>> = async (data) => {
-        // console.log(data)
+        console.log(data)
         try {
-            const response = await axios.post(`/api/cards`, data);
-            console.log('Server Response:', response.data);
+            const response = await fetch(`${API}/cards`,{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            console.log('Server Response:', await response.json());
         } catch (error) {
             console.error('There was an error!', error);
         }
